7648a60ee4239168141319f54aa61befa92b3364
USER_NAME <147196596+tanishka786@users.noreply.github.com>
2025-12-08 23:53:17 +0530

    made few changes

---
+++ 4-GuessTheNumber/script.js
@@ -0,0 +1,5 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
